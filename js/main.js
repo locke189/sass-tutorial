438fe7cdbf6f387f7542772a5eade77b956ee8b8
Juan Insuasti <jinsuasti@hugeinc.com>
2019-08-23 10:35:21 -0400

    afterclass comments

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 import '../styles/base.scss'
+import hero from '../templates/hero.html'
+import colorSamples from '../templates/colorSamples.html'
+
+const components = [
+  hero,
+  colorSamples
+]
+
+const main = document.querySelector('.main')
+const fragment = document.createDocumentFragment();
+
+components.forEach((component) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = component
+  fragment.appendChild(wrapper)
+})
+
+main.appendChild(fragment)
